c30ecba4e1277b7b34b1a177441ceb87a419d18c
Bret Baker <bretbaker808@gmail.com>
2019-07-09 15:51:11 -0700

    questions done, need to start timer and extra effects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,15 @@
 $(window).on('load', function() {
 
-    // hide question 1 so it can be revealed once start button is clicked
-    // $("#question-1").hide();
-    $("#start-button").hide();
+    // hide questions so they can be revealed sequentially
+    $("#question-1").hide();
+    $("#question-2").hide();
+    $("#question-3").hide();
+    $("#question-4").hide();
+    $("#question-5").hide();
+    $("#question-6").hide();
+    $("#question-7").hide();
+    $("#question-8").hide();
+    
 
 
 });
@@ -12,12 +19,47 @@ $(window).on('load', function() {
 
 
 
-// $(document).ready(function() {
+$(document).ready(function() {
 
-//     // hide start button once clicked
-//     $("#start-button").on('click', function() {
-//         $("#start-button").hide();
-//         $("#question-1").show();
-//     })
+    // hide start button once clicked
+    $("#start-button").on('click', function() {
+        $("#start-button").hide();
+        $("#question-1").show();
+    });
 
-// });
+    $("#correct-1").on('click', function() {
+        $("#question-1").hide();
+        $("#question-2").show();
+    });
+
+    $("#correct-2").on('click', function() {
+        $("#question-2").hide();
+        $("#question-3").show();
+    });
+
+    $("#correct-3").on('click', function() {
+        $("#question-3").hide();
+        $("#question-4").show();
+    });
+
+    $("#correct-4").on('click', function() {
+        $("#question-4").hide();
+        $("#question-5").show();
+    });
+
+    $("#correct-5").on('click', function() {
+        $("#question-5").hide();
+        $("#question-6").show();
+    });
+
+    $("#correct-6").on('click', function() {
+        $("#question-6").hide();
+        $("#question-7").show();
+    });
+
+    $("#correct-7").on('click', function() {
+        $("#question-7").hide();
+        $("#question-8").show();
+    });
+
+});
