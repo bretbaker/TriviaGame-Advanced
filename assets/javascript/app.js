cd2ba102239564236643397e5463d76bd56c17c8
Bret Baker <bretbaker808@gmail.com>
2019-07-09 14:57:52 -0700

    basic question design/question 1 complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(window).on('load', function() {
+
+    // hide question 1 so it can be revealed once start button is clicked
+    // $("#question-1").hide();
+    $("#start-button").hide();
+
+
+});
+
+
+
+
+
+
+// $(document).ready(function() {
+
+//     // hide start button once clicked
+//     $("#start-button").on('click', function() {
+//         $("#start-button").hide();
+//         $("#question-1").show();
+//     })
+
+// });
